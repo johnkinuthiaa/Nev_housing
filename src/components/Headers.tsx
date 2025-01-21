35e6a27089dd445dc3711b8db065ee86a930bdc1
johnkinuthiaa <kinuthiajohn122@gmail.com>
2025-01-22 00:39:05 +0300

    card

--- src/components/Headers.tsx
+++ src/components/Headers.tsx
@@ -4,9 +4,11 @@ import {CgProfile} from "react-icons/cg";
 const Headers =()=>{
     const navigate:NavigateFunction =useNavigate();
     return(
-        <header className={"flex justify-between p-7 shadow-2xl w-full "}>
-            <div>Home</div>
-            <div className={"rounded-full bg-white text-black flex gap-7 p-6 justify-between font-bold cursor-pointer"}>
+        <header className={"flex justify-between p-9 shadow-2xl w-full content-center"}>
+            <div className={"text-4xl font-extrabold"}>
+                <h1 className={"text-[#ff914d]"}>Nevani <span className={"text-[#5271ff] border-l-2 border-red-500"}>Housing</span></h1>
+            </div>
+            <div className={"rounded-full bg-white text-black flex gap-7 justify-between font-bold cursor-pointer"}>
                 <p onClick={()=>navigate("/")}>Home</p>
                 <p onClick={()=>navigate("/properties")}>Properties</p>
                 <p>Service</p>
